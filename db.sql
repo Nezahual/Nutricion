29e88e37a24083c1496cefd319601badcce24336
USUARIO <USUARIO@Aula-Pc>
2018-02-07 11:23:34 +0100

    db

--- db.sql
+++ db.sql
@@ -1,5 +1,12 @@
 create database nutricion;
 use nutricion;
+create table usuario (
+
+	username varchar(15),
+	nombre_completo varchar(100),
+	password varchar(15),
+	primary key (username)
+);
 
 create table plato (
 
@@ -11,13 +18,34 @@ create table plato (
 	foreign key (autor) references usuario(username)
 );
 
-create table usuario (
 
-	username varchar(15),
-	nombre_completo varchar(100),
-	password varchas(15),
-	primary key (username)
-);
+ CREATE TABLE `ingredientes` (
+  id int not null auto_increment,
+  `descripcion` VARCHAR(60), 
+  `agua` DOUBLE NULL DEFAULT 0, 
+  `energia` INTEGER DEFAULT 0, 
+  `proteinas` DOUBLE NULL DEFAULT 0, 
+  `grasas` DOUBLE NULL DEFAULT 0, 
+  `carbohidratos` DOUBLE NULL DEFAULT 0, 
+  `fibra` DOUBLE NULL DEFAULT 0,
+  `azucar` DOUBLE NULL DEFAULT 0, 
+  `calcio` INTEGER DEFAULT 0, 
+  `hierro` DOUBLE NULL DEFAULT 0, 
+  `magnesio` DOUBLE NULL DEFAULT 0, 
+  `fosforo` INTEGER DEFAULT 0, 
+  `potasio` INTEGER DEFAULT 0, 
+  `sodio` INTEGER DEFAULT 0, 
+  `vitamina_c` DOUBLE NULL DEFAULT 0, 
+  `vitamina_b12` DOUBLE NULL DEFAULT 0, 
+  `vitamina_a` INTEGER DEFAULT 0, 
+  `vitamina_e` DOUBLE NULL DEFAULT 0, 
+  `vitamina_d` DOUBLE NULL DEFAULT 0, 
+  `grasas_saturadas` DOUBLE NULL DEFAULT 0, 
+  `grasas_mono` DOUBLE NULL DEFAULT 0, 
+  `grasas_poli` DOUBLE NULL DEFAULT 0, 
+  `colesterol` INTEGER DEFAULT 0,
+  primary key(id)
+) ENGINE=innoDB DEFAULT CHARSET=utf8 collate utf8_spanish_ci;
 
 create table plato_ingrediente(
 
